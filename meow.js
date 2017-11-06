857e307d788a047e00f9fb0e3c4390740190c4f3
Lordgreed1994 <lordgreed1994@gmail.com>
2017-11-06 12:03:50 -0700

    basic xhr working. returning a payload

---
+++ meow.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    console.log("READY!!! MEOW!!!");
+
+    var searchGiphyButton = document.getElementById("getMeow")
+    ;
+    searchGiphyButton.addEventListener("click", function() {
+        console.log("Meow Click Click!!!");
+    });
+};
